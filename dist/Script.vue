7ff9e79be66cf72c49eb38464e7f814b8b8bc459
Drkrai <114841316+Drkrai@users.noreply.github.com>
2023-10-12 19:15:03 +0800

    renz

---
+++ dist/Script.vue
@@ -0,0 +1,30 @@
+
+<script>
+export default {
+  name: "ProductView",
+ mounted() {
+    this.loadScripts();
+},
+methods: {
+    loadScripts() {
+      const scriptUrls = [
+        '@/assets/assets/vendor/jquery/jquery.min.js',
+        '@/assets/assets/vendor/bootstrap/js/bootstrap.min.js',
+        '@/assets/assets/js/isotope.min.js',
+        '@/assets/assets/js/owl-carousel.js',
+        '@/assets/assets/js/counter.js',
+        '@/assets/assets/js/custom.js',
+      ];
+
+      const head = document.getElementsByTagName('head')[0];
+
+      scriptUrls.forEach((scriptUrl) => {
+        const script = document.createElement('script');
+        script.src = scriptUrl;
+        script.async = true;
+        head.appendChild(script);
+      });
+    },
+ },
+};
+</script>
